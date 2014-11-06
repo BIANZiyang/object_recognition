d629b5fbd32f45878604ae6fb20d3ce3ffa917fe
ras <ras@nuc.(none)>
2014-11-06 17:26:29 +0100

    Fixed pointcloud to cv::Mat

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -7,6 +7,8 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
 
@@ -30,10 +32,9 @@ public:
 
     }
     void imageCb(const sensor_msgs::PointCloud2ConstPtr& pcl_msg) {
-
-//        sensor_msgs::PointCloud2 filtered_pcl;
-//        filterHSV(pcl_msg, filtered_pcl);
-//        vis_pub.publish(filtered_pcl);
+        sensor_msgs::PointCloud2 filtered_pcl;
+        filterHSV(pcl_msg, filtered_pcl);
+        vis_pub.publish(filtered_pcl);
     }
 
 private:
@@ -63,11 +64,17 @@ private:
             }
         }
 
+        ROS_INFO("pcl_msg size: %d RGBMat size: (%d, %d)", input->size(), RGBMat.rows, RGBMat.cols);
 
-        cv::Scalar lower(-30, 0, 0);
-        cv::Scalar upper(30, 255, 255);
-        cv::cvtColor(RGBMat, HSVMat, CV_RGB2HSV);
+
+        cv::Scalar lower(207/2-20, 45*2.55, 70*2.55);
+        cv::Scalar upper(207/2+20, 55*2.55, 80*2.55);
+        cv::cvtColor(RGBMat, HSVMat, CV_BGR2HSV);
         cv::inRange(HSVMat, lower, upper, RGBMat);
+
+
+        cv::imshow("Display window", RGBMat);
+        cv::waitKey(1);
 
 
 
@@ -83,6 +90,10 @@ private:
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "object_detection");
+    cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE);
+//    cv::Mat RGBMat = cv::Mat::ones(480, 640, CV_8UC3);
+//    cv::imshow("Display window", RGBMat);
+//    cv::waitKey(0);
     object_detection od;
     ros::spin();
 
