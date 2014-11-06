5d9f8b2a1e57141fafb589702df130c300a59547
ras <ras@nuc.(none)>
2014-11-06 12:43:47 +0100

    started working on HSV filtering

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -1,1 +1,89 @@
- 
+// Copyed from Hand_tracker
+#include <cstdio>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
+
+typedef pcl::PCLPointCloud2 Cloud2;
+typedef pcl::PointXYZRGB Point;
+typedef pcl::PointCloud<Point> Cloud;
+typedef pcl::PointXYZHSV PointHSV;
+typedef pcl::PointCloud<PointHSV> CloudHSV;
+
+class object_detection{
+public:
+    object_detection() {
+        pcl_sub = nh.subscribe("/camera/depth_registered/points", 1, &object_detection::imageCb, this);
+        vis_pub = nh.advertise<sensor_msgs::PointCloud2>("/object_recognition/deteciton", 1);
+        hmin=0;
+        hmax=120;
+        smin=0;
+        smax=255;
+        vmin=0;
+        vmax=255;
+
+    }
+    void imageCb(const sensor_msgs::PointCloud2ConstPtr& pcl_msg) {
+
+//        sensor_msgs::PointCloud2 filtered_pcl;
+//        filterHSV(pcl_msg, filtered_pcl);
+//        vis_pub.publish(filtered_pcl);
+    }
+
+private:
+    void filterHSV(const sensor_msgs::PointCloud2ConstPtr& pcl_msg, sensor_msgs::PointCloud2& filtered){
+        Cloud::Ptr input(new Cloud);
+        Cloud::Ptr output(new Cloud);
+        Cloud2 tmp_pcl;
+        pcl_conversions::toPCL(*pcl_msg, tmp_pcl);
+        pcl::fromPCLPointCloud2(tmp_pcl, *input);
+
+        cv::Mat RGBMat, HSVMat;
+        if (input->isOrganized()) {
+            RGBMat = cv::Mat(input->height, input->width, CV_8UC3);
+
+            if (!input->empty()) {
+                for (int h=0; h<RGBMat.rows; h++) {
+                    for (int w=0; w<RGBMat.cols; w++) {
+                        Point point = input->at(w, h);
+
+                        Eigen::Vector3i rgb = point.getRGBVector3i();
+
+                        RGBMat.at<cv::Vec3b>(h,w)[0] = rgb[2];
+                        RGBMat.at<cv::Vec3b>(h,w)[1] = rgb[1];
+                        RGBMat.at<cv::Vec3b>(h,w)[2] = rgb[0];
+                    }
+                }
+            }
+        }
+
+
+        cv::Scalar lower(-30, 0, 0);
+        cv::Scalar upper(30, 255, 255);
+        cv::cvtColor(RGBMat, HSVMat, CV_RGB2HSV);
+        cv::inRange(HSVMat, lower, upper, RGBMat);
+
+
+
+//        pcl::toROSMsg(*output, filtered);
+    }
+
+    float hmin, smin, vmin, hmax, smax, vmax;
+    ros::Subscriber pcl_sub;
+    ros::Publisher vis_pub;
+    ros::NodeHandle nh;
+
+};
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "object_detection");
+    object_detection od;
+    ros::spin();
+
+}
